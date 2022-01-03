91ba32dc9e02745bfbdad8a5534d354698b2179e
Ryusei-0407 <ryu000407@gmail.com>
2022-01-03 12:13:53 +0900

    delete packages

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,3 @@
-<script>
-	import { initializeApollo } from '../lib/apolloClient';
-	import { GET_USERS } from '../queries/schema';
-	import { setClient, query } from 'svelte-apollo';
-
-	const client = initializeApollo();
-	setClient(client);
-
-	const data = query(GET_USERS);
-	$: console.log(data);
-
-	const text = 'Svelte';
-</script>
-
-<div>
-	<h1>Hello, {text}!</h1>
-</div>
+<h1>
+    Hello, Svelte
+</h1>
