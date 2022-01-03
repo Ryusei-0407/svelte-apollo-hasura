7440ce2879e9458fa7ac61d33faa7d328bbef710
Ryusei-0407 <ryu000407@gmail.com>
2022-01-03 11:55:03 +0900

    wip

---
+++ src/lib/apolloClient.ts
@@ -0,0 +1,16 @@
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+
+const createApolloClient = () => {
+	return new ApolloClient({
+		link: new HttpLink({
+			uri: 'http://localhost:8080/v1/graphql'
+		}),
+		cache: new InMemoryCache()
+	});
+};
+
+export const initializeApollo = () => {
+	const _apolloClient = createApolloClient();
+
+	return _apolloClient;
+};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { initializeApollo } from '../lib/apolloClient';
+	import { GET_USERS } from '../queries/schema';
+	import { setClient, query } from 'svelte-apollo';
+
+	const client = initializeApollo();
+	setClient(client);
+
+	const data = query(GET_USERS);
+	$: console.log(data);
+
+	const text = 'Svelte';
+</script>
+
+<div>
+	<h1>Hello, {text}!</h1>
+</div>
