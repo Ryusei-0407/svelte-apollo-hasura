ebade0334f544c44f16df231dc22a8f6bc804e51
Ryusei-0407 <ryu000407@gmail.com>
2022-01-05 15:18:27 +0900

    add create todo query

--- src/lib/queries/query.ts
+++ src/lib/queries/query.ts
@@ -2,10 +2,22 @@ import { gql } from '@apollo/client/core';
 
 export const GET_TODOS = gql`
   query GetTodos {
-    todo {
+    todo (order_by: { created_at: desc } ) {
       id
       task
       is_completed
+    }
+  }
+`
+
+export const ADD_TODO = gql`
+  mutation AddTodo ($task: String!) {
+    insert_todo_one(object: {task: $task}) {
+      task
+      id
+      is_completed
+      created_at
+      updated_at
     }
   }
 `
