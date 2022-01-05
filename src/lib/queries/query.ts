7d905c52f2da1918cb98fbb047a404ce92296e66
Ryusei-0407 <ryu000407@gmail.com>
2022-01-05 18:04:56 +0900

    add change completed_state and delete task process

--- src/lib/queries/query.ts
+++ src/lib/queries/query.ts
@@ -1,23 +1,57 @@
 import { gql } from '@apollo/client/core';
 
 export const GET_TODOS = gql`
-  query GetTodos {
-    todo (order_by: { created_at: desc } ) {
-      id
-      task
-      is_completed
-    }
-  }
-`
+	query GetTodos {
+		todo(order_by: { created_at: desc }) {
+			id
+			task
+			is_completed
+		}
+	}
+`;
 
 export const ADD_TODO = gql`
-  mutation AddTodo ($task: String!) {
-    insert_todo_one(object: {task: $task}) {
-      task
-      id
-      is_completed
-      created_at
-      updated_at
-    }
-  }
-`
+	mutation AddTodo($task: String!) {
+		insert_todo_one(object: { task: $task }) {
+			task
+		}
+	}
+`;
+
+export const EDIT_TODO = gql`
+	mutation UpdateTodo($id: uuid!, $task: String!) {
+		update_todo_by_pk(_set: { task: $task }, pk_columns: { id: $id }) {
+			id
+			task
+		}
+	}
+`;
+
+export const COMPLETE_TASK = gql`
+	mutation ($id: uuid!) {
+		update_todo_by_pk(_set: { is_completed: true }, pk_columns: { id: $id }) {
+			id
+			task
+			is_completed
+		}
+	}
+`;
+
+export const UNCOMPLETE_TASK = gql`
+	mutation ($id: uuid!) {
+		update_todo_by_pk(_set: { is_completed: false }, pk_columns: { id: $id }) {
+			id
+			task
+			is_completed
+		}
+	}
+`;
+
+export const DELETE_TASK = gql`
+	mutation ($id: uuid!) {
+		delete_todo_by_pk(id: $id) {
+			id
+			task
+		}
+	}
+`;
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,42 +1,130 @@
-<script>
-  import { Circle2 } from 'svelte-loading-spinners';
-  import { createApolloClient } from '../lib/apolloClient';
-  import { GET_TODOS } from '../lib/queries/query';
+<script lang="ts">
+	import { Circle2 } from 'svelte-loading-spinners';
+	import { createApolloClient } from '../lib/apolloClient';
+	import {
+		GET_TODOS,
+		ADD_TODO,
+		EDIT_TODO,
+		COMPLETE_TASK,
+		UNCOMPLETE_TASK,
+		DELETE_TASK
+	} from '../lib/queries/query';
 
-  const getTodos = async () => {
-    const client = createApolloClient();
+	const getTodos = async () => {
+		const client = createApolloClient();
 
-    const res = await client.query({ query: GET_TODOS });
-    const data = await res.data;
+		const res = await client.query({ query: GET_TODOS, fetchPolicy: 'network-only' });
+		const data = await res.data;
 
-    return await data;
-  };
-  let promise = getTodos();
+		return await data;
+	};
+	let promise = getTodos();
 
+	let task = '';
+
+	const createTodo = async (task) => {
+		const client = createApolloClient();
+
+		const res = await client.mutate({ mutation: ADD_TODO, variables: { task } });
+		console.log(res);
+		let _promise = getTodos();
+		promise = _promise;
+	};
+
+	/*
+    const editTodo = async (id: String, task: String) => {
+      const client = createApolloClient();
+
+      const res = await client.mutate({  });
+    }
+  */
+
+	const update_task = async (id, is_completed) => {
+		const client = createApolloClient();
+
+		if (is_completed) {
+			const res = await client.mutate({ mutation: UNCOMPLETE_TASK, variables: { id } });
+			console.log(res);
+		} else {
+			const res = await client.mutate({ mutation: COMPLETE_TASK, variables: { id } });
+			console.log(res);
+		}
+		let _promise = getTodos();
+		promise = _promise;
+	};
+
+	const delete_task = async (id) => {
+		const client = createApolloClient();
+
+		const res = await client.mutate({ mutation: DELETE_TASK, variables: { id } });
+		console.log(res);
+		let _promise = getTodos();
+		promise = _promise;
+	};
 </script>
 
 <div class="flex justify-center items-center">
-  <h1 class="text-red-500 font-mono text-2xl py-3">Hello, Svelte</h1>
+	<h1 class="text-red-500 font-mono text-2xl py-3">Hello, Svelte</h1>
 </div>
 
-{#await promise}
-  <div class="flex justify-center items-center py-5">
-    <Circle2 size="100" />
-  </div>
-{:then data}
-  {#each data.todo as todo}
-    <div class="flex justify-center items-center">
-      <ul class="font-mono">
-        {#if !todo.is_completed}
-          <li>{todo.task}</li>
-        {:else}
-          <li class="line-through">{todo.task}</li>
-        {/if}
-      </ul>
-    </div>
-  {/each}
-{:catch error}
-  <div class="flex justify-center items-center">
-    <p>{error.message}</p>
-  </div>
-{/await}
+<div class="flex justify-center items-center py-3">
+	<input class="border-2 border-red-500 rounded font-mono py-0.5" type="text" bind:value={task} />
+	<button
+		class="bg-red-500 hover:bg-red-700 text-white font-mono ml-3 py-1 px-3 rounded"
+		disabled={!task}
+		on:click={createTodo(task)}>Add Todo</button
+	>
+</div>
+
+<div class="py-5">
+	{#await promise}
+		<div class="flex justify-center items-center py-5">
+			<Circle2 size="100" />
+		</div>
+	{:then data}
+		{#each data.todo as todo}
+			<div class="flex justify-center items-center w-full">
+				<table class="font-mon w-full">
+					<tbody class="flex justify-center items-center border">
+						<tr>
+							{#if !todo.is_completed}
+								<td class="px-10 w-60">{todo.task}</td>
+							{:else}
+								<td class="line-through px-10 w-60">{todo.task}</td>
+							{/if}
+							<td>
+								<button
+									class="bg-red-500 hover:bg-red-700 text-white text-sm font-mono ml-2 px-2 rounded"
+									>Edit</button
+								>
+							</td>
+							<td>
+								{#if todo.is_completed}
+									<button
+										class="bg-red-500 hover:bg-red-700 text-white text-sm font-mono ml-2 px-2 rounded"
+										on:click={update_task(todo.id, true)}>UnCompleted</button
+									>
+								{:else}
+									<button
+										class="bg-red-500 hover:bg-red-700 text-white text-sm font-mono ml-2 px-4 rounded"
+										on:click={update_task(todo.id, false)}>Completed</button
+									>
+								{/if}
+							</td>
+							<td>
+								<button
+									class="bg-red-500 hover:bg-red-700 text-white text-sm font-mono ml-2 px-2 rounded"
+									on:click={delete_task(todo.id)}>Delete</button
+								>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		{/each}
+	{:catch error}
+		<div class="flex justify-center items-center">
+			<p>{error.message}</p>
+		</div>
+	{/await}
+</div>
