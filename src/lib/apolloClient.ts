801784f73f36a8d1e83e771c09bc6e2bc33b52da
Ryusei-0407 <ryu000407@gmail.com>
2022-01-05 18:04:51 +0900

    format

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,8 +1,8 @@
-import { ApolloClient, InMemoryCache, } from '@apollo/client/core';
+import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 
 export const createApolloClient = () => {
-  return new ApolloClient({
-    uri: 'http://localhost:8080/v1/graphql',
-    cache: new InMemoryCache()
-  });
-}
+	return new ApolloClient({
+		uri: 'http://localhost:8080/v1/graphql',
+		cache: new InMemoryCache()
+	});
+};
