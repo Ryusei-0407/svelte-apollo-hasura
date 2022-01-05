2dd02d674d7cf71d4c683875e164435aa0ed5d85
Ryusei-0407 <ryu000407@gmail.com>
2022-01-05 12:47:52 +0900

    fetch query

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,22 @@
-<h1>
-    Hello, Svelte
-</h1>
+<script>
+	import { ApolloClient, InMemoryCache, gql } from '@apollo/client/core';
+
+	const client = new ApolloClient({
+		uri: 'http://localhost:8080/v1/graphql',
+		cache: new InMemoryCache()
+	});
+
+  client.query({
+    query: gql`
+      query GetBooks {
+        book {
+          id
+          name
+        }
+      }
+    `
+  }).then(res => console.log(res.data.book));
+
+</script>
+
+<h1>Hello, Svelte</h1>
